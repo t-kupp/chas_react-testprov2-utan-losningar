99bc038078be3f78ea36f669fd99787a6b1fe22e
Jan <kupper.thorge@gmail.com>
2025-02-26 15:18:11 +0100

    q2 done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./question1/App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './question2/App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- src/question2/App.jsx
+++ src/question2/App.jsx
@@ -3,12 +3,44 @@
 // Från början visas användarens nuvarande e-postadress.
 // Bredvid e-postadressen ska det finnas en "Redigera"-knapp.
 
+import { useState } from 'react';
+
 // När "Redigera" klickas, visa ett inputfält för att skriva in en ny e-postadress
 // och byt "Redigera"-knappen till en "Spara"-knapp.
 // När "Spara" klickas, uppdatera e-postadressen och visa den uppdaterade adressen.
 
 function App() {
-  return <div></div>;
+  const [email, setEmail] = useState('user@google.com');
+  const [isEditing, setIsEditing] = useState(false);
+  const [newEmail, setNewEmail] = useState('');
+
+  return (
+    <div className='flex items-center gap-2 h-screen p-8'>
+      {!isEditing ? (
+        <>
+          <p className='w-48'>{email}</p>
+          <button onClick={() => setIsEditing(true)}>Change</button>
+        </>
+      ) : (
+        <>
+          <input
+            className='border rounded w-48'
+            onChange={(e) => setNewEmail(e.target.value)}
+            type='text'
+            defaultValue={email}
+          />
+          <button
+            onClick={(e) => {
+              setEmail(newEmail);
+              setIsEditing(false);
+            }}
+          >
+            Save
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
